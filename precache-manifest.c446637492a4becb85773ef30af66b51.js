f980995de8c45286960b87e2fed18e2e07492857
NelliPestych <NelliPestych@bigmir.net>
2020-07-05 18:35:01 +0300

    Updates

---
+++ precache-manifest.c446637492a4becb85773ef30af66b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed12d2cd271ea3bdb00dea9bc2f8194",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "fe1f76e486fea4c5d48e",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c5021d4487836c636377",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3577c5fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3577c5fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1f76e486fea4c5d48e",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.f992ac7e.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
